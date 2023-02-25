1b97da11931e719dc0105ac2d558666613bf6327
kakd20061 <gizard.biznes123@vp.pl>
2023-02-25 15:19:49 +0100

    add sorting by price

---
+++ CryptoStatistics/CryptoStatistics/Enums/OrderByStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoStatistics.Enums
+{
+    public enum OrderByStatusEnum 
+    { 
+        Ascending = 1,
+        Descending = 2,
+        Default = 3
+    }
+
+}
--- CryptoStatistics/CryptoStatistics/Statistics.xaml.cs
+++ CryptoStatistics/CryptoStatistics/Statistics.xaml.cs
@@ -5,16 +5,21 @@ using Xamarin.Forms;
 using CryptoStatistics.Models;
 using CryptoStatistics.Services;
 using System.Linq;
+using CryptoStatistics.Enums;
 
 namespace CryptoStatistics
 {
     public partial class Statistics : ContentPage
     {
         private ICollection<CryptoCurrencyData> ApiData;
+        private OrderByStatusEnum OrderByStatus;
         public Statistics()
         {
             InitializeComponent();
             GetApiData();
+
+            OrderByStatus = OrderByStatusEnum.Default;
+            sortButton.Text = $"Sort: {OrderByStatus}";
         }
 
         private string GetPickerData()
@@ -26,7 +31,39 @@ namespace CryptoStatistics
         {
             var response = await ApiService.GetDataFromApiAsync<AssetsApiResponseModel>("https://api.coincap.io/v2/assets/", new Dictionary<string, string>() { { "limit", GetPickerData() } });
             ApiData = response.Data.ToList();
-            listView.ItemsSource = response.Data;
+
+            listView.ItemsSource = !string.IsNullOrEmpty(searchBar.Text) ? 
+                ApiData.Where(n => n.Name.ToLower().Contains(searchBar.Text.ToLower())) : ApiData;
+        }
+
+        private List<CryptoCurrencyData> SortedData()
+        {
+            if(OrderByStatus == OrderByStatusEnum.Default)
+            {
+                var result = !string.IsNullOrEmpty(searchBar.Text) ?
+                    ApiData.Where(n => n.Name.ToLower().Contains(searchBar.Text.ToLower())).OrderBy(n => n.PriceUsdD).ToList() :
+                    ApiData.OrderBy(n => n.PriceUsdD).ToList();
+
+                OrderByStatus = OrderByStatusEnum.Ascending;
+                return result;
+            }
+            else if(OrderByStatus == OrderByStatusEnum.Ascending)
+            {
+                var result = !string.IsNullOrEmpty(searchBar.Text) ?
+                    ApiData.Where(n => n.Name.ToLower().Contains(searchBar.Text.ToLower())).OrderByDescending(n => n.PriceUsdD).ToList() :
+                    ApiData.OrderByDescending(n => n.PriceUsdD).ToList();
+
+                OrderByStatus = OrderByStatusEnum.Descending;
+                return result;
+            }
+            else
+            {
+                List<CryptoCurrencyData> result = !string.IsNullOrEmpty(searchBar.Text) ?
+                    ApiData.Where(n => n.Name.ToLower().Contains(searchBar.Text.ToLower())).ToList() : ApiData.ToList();
+
+                OrderByStatus = OrderByStatusEnum.Default;
+                return result;
+            }
         }
 
         private void SearchData(object sender, TextChangedEventArgs e)
@@ -43,8 +80,18 @@ namespace CryptoStatistics
         private void RefreshData(object sender, EventArgs e)
         {
             GetApiData();
-            listView.ItemsSource = ApiData;
+
+            OrderByStatus = OrderByStatusEnum.Default;
+            sortButton.Text = $"Sort: {OrderByStatus}";
+
             listView.EndRefresh();
+        }
+
+        private void SortData(object sender, EventArgs e)
+        {
+            listView.ItemsSource = SortedData();
+
+            sortButton.Text = $"Sort: {OrderByStatus}";
         }
     }
 }
